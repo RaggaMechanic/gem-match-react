c34146218a7e46fecf50bc29627fa05f8078c6be
RaggaMechanic <10571404+RaggaMechanic@users.noreply.github.com>
2022-08-14 04:50:13 +0300

    worked gem generation, worked gem match, worked gem drops, worked grid size configuration

--- src/App.js
+++ src/App.js
@@ -1,21 +1,114 @@
+import React, { useState } from "react";
 import "./styles.css";
 
-const arr = Array.from({ length: 25 }, (e, i) => getNextRand(1, 5));
+class Gem {
+  x = -1;
+  y = -1;
+  type = 0;
+  constructor(x, y, type) {
+    this.x = x;
+    this.y = y;
+    this.type = type;
+  }
+}
+
+const size = 8;
+
+function createGems() {
+  let x = 0,
+    y = 0;
+  return Array.from({ length: size * size }, (e, i) => {
+    if (x === size) {
+      x = 0;
+      y++;
+    }
+    const gem = new Gem(x, y, getNextRand(1, 5));
+    x++;
+    return gem;
+  });
+}
+
+function updateGridWithMatches(gems) {
+  const matches = matchLines(gems);
+  if (matches.length > 0) {
+    const newGems = Object.assign([], gems);
+    matches.flat().forEach((m, i) => {
+      const gem = newGems.find((g) => g.x === m.x && g.y === m.y);
+      if (gem) gem.type = 0; // getNextRand(1, 5)
+    });
+    return { needUpdate: true, newGems };
+  }
+
+  return { needUpdate: false, newGems: gems };
+}
 
 export default function App() {
-  const grid = arr.map((el, i) => {
+  const [gems, setGems] = useState([]);
+
+  const newGame = () => {
+    console.log("newGame call");
+    const nGems = createGems();
+    setGems(nGems);
+    setTimeout(() => updateGems(nGems), 200);
+  };
+
+  const dropGems = (gems) => {
+    const swaps = getSwaps(gems);
+    console.log("dropGems swaps:", swaps);
+    swaps.forEach((s) => {
+      const _gems = Object.assign([], gems);
+      const gem = _gems.find((g) => g.x === s.from.x && g.y === s.from.y);
+      const hole = _gems.find((g) => g.x === s.to.x && g.y === s.to.y);
+      gem.type = s.to.type;
+      hole.type = s.from.type;
+      setGems(_gems);
+    });
+  };
+
+  const updateGems = (gems) => {
+    console.log("updateGems call", gems);
+    const { needUpdate, newGems } = updateGridWithMatches(gems);
+    if (needUpdate) {
+      console.log("updateGems setGems", newGems);
+      setGems(newGems);
+      setTimeout(() => {
+        dropGems(newGems);
+        setTimeout(() => updateGems(gems), 200);
+      }, 500);
+    }
+  };
+
+  const grid = gems.map((el, i) => {
+    const key = `${el.x}${el.y}`;
     const gm = (
       <span
-        key={i}
-        className={"gem " + colorByNum(el)}
-        onClick={(event) => clickGem(event, i, el)}
+        key={key}
+        className={"gem " + colorByNum(el.type)}
+        onClick={(event) => clickGem(event, i, el, () => updateGems(gems))}
       >
-        &nbsp;
+        {key}
       </span>
     );
     return gm;
   });
-  return <div className="App gems">{grid}</div>;
+
+  React.useEffect(() => {
+    if (gems.length === 0) newGame();
+  });
+
+  const style = {
+    gridTemplateColumns: `repeat(${size}, 50px)`,
+    gridTemplateRows: `repeat(${size}, 50px)`
+  };
+
+  return (
+    <div className="App">
+      <div className="gems" style={style}>
+        {grid}
+      </div>
+      <button onClick={newGame}>newGame</button>
+    </div>
+  );
 }
 
 function colorByNum(num) {
@@ -41,12 +134,100 @@ function getNextRand(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function clickGem(event, index, gem) {
+function clickGem(event, index, gem, updateGems) {
   console.log(event, index, gem);
-  matchGems();
+  updateGems();
 }
 
-function matchGems() {
-  const gemsLines = [];
-  arr.forEach((gem, i) => {});
+function separateGems(gems) {
+  let lines = [],
+    cols = [];
+  for (let i = 0; i < size; i++) {
+    cols.push(gems.filter((g) => g.x === i));
+    lines.push(gems.filter((g) => g.y === i));
+  }
+  return { lines, cols };
+}
+
+// simple
+function matchLines(gems) {
+  const matches = [];
+  const { lines, cols } = separateGems(gems);
+  const all = lines.concat(cols);
+
+  all.forEach((line) => {
+    let match = [];
+    line.forEach((gem, i) => {
+      const someType = (match[match.length - 1] || {}).type === gem.type;
+      const last = i === line.length - 1;
+
+      if (gem.type === 0) {
+        if (match.length > 2) {
+          matches.push(match);
+        }
+        match = [];
+        return;
+      }
+
+      if (match.length === 0) {
+        match = [gem];
+        return;
+      }
+
+      if (someType) {
+        match.push(gem);
+      } else if (match.length > 2) {
+        matches.push(match);
+        match = [gem];
+      } else {
+        match = [gem];
+      }
+
+      if (match.length > 2 && last) {
+        matches.push(match);
+      }
+    });
+  });
+
+  return matches;
+}
+
+function getSwaps(gems) {
+  const _gems = Object.assign([], gems);
+  const { cols } = separateGems(_gems);
+
+  const canDrop = (col) => {
+    const firstGemIndex = col.findIndex((g) => g.type !== 0);
+    const holeAfterGemIndex = col.findLastIndex(
+      (g, i) => g.type === 0 && i > firstGemIndex
+    );
+    console.log(
+      "holes",
+      col.filter((g) => g.type === 0)
+    );
+    console.log(firstGemIndex, holeAfterGemIndex);
+    return firstGemIndex > -1 && holeAfterGemIndex > -1;
+  };
+
+  const swaps = [];
+  cols.forEach((col) => {
+    console.log(col, canDrop(col));
+    while (canDrop(col)) {
+      const lastHoleIndex = col.findLastIndex((g) => g.type === 0);
+      const gemBeforeHoleIndex = col.findLastIndex(
+        (g, i) => g.type !== 0 && i < lastHoleIndex
+      );
+      const hole = col[lastHoleIndex];
+      const gem = col[gemBeforeHoleIndex];
+      const swap = {
+        from: { x: gem.x, y: gem.y, type: gem.type },
+        to: { x: hole.x, y: hole.y, type: hole.type }
+      };
+      hole.type = swap.from.type;
+      gem.type = swap.to.type;
+      console.log("swap", swap);
+      swaps.push(swap);
+    }
+  });
+  return swaps;
 }
